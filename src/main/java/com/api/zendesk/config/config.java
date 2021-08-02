b9494dc6c3302d14b5bfe8438c7d78e45175a784
Akshay <guptaakshay908@gmail.com>
2021-08-02 15:48:14 -0500

    Dotenv .env file

--- src/main/java/com/api/zendesk/config/config.java
+++ src/main/java/com/api/zendesk/config/config.java
@@ -1,34 +1,25 @@
 package com.api.zendesk.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
-
-import javax.annotation.Resource;
 
 @Configuration
 public class config {
 
-    @Autowired
-    public Environment env;
 
+    Dotenv dotenv = Dotenv
+            .configure()
+            .load();
 
-     //System.out.println("username is "+env.getProperty("JAVA_HOME"));
-    //String username = env.getProperty("ZEN_USER");
-
-
-    //String password = env.getProperty("ZEN_PASSWORD");
-    //Authentication to access zendesk account
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
 
 
-        return builder.basicAuthentication(env.getProperty("ZEN_USER"),
-                env.getProperty("ZEN_PASSWORD"))
+        return builder.basicAuthentication(dotenv.get("ZEN_USER"),
+                dotenv.get("ZEN_PASSWORD"))
                 .build();
     }
 
